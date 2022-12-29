0a8042f58b769e89ba1629a2073cbf3de76d2424
Alberto Gómez Juan <agomezjuan@hotmail.com>
2022-12-29 13:48:38 +0000

    add motion

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import axios from 'axios'
 import { FaTwitter } from 'react-icons/fa'
 import './App.css'
 import Spinner from './components/Spinner'
+import { Motion, spring } from 'react-motion';
 
 function App() {
 
@@ -45,12 +46,20 @@ function App() {
   console.log(randomQuote);
   return (
     <div id="quote-box" className='card quote'>
-      {quotes.length == 0 ? <Spinner /> : (<div>
-        <blockquote>
-          <p id="text">"{randomQuote?.quote}"</p>
-          <small id="author">{randomQuote?.author}</small>
-        </blockquote>
-      </div>)}
+      {quotes.length == 0 ? <Spinner /> : (<Motion defaultStyle={{ x: 0 }} style={{ x: spring(randomQuote ? 1 : 0) }}>
+        {({ x }) => (
+
+          <div style={{
+            transform: `scale(${x})`,
+            transformOrigin: 'top left'
+          }}>
+            <blockquote>
+              <p id="text">"{randomQuote?.quote}"</p>
+              <small id="author">{randomQuote?.author}</small>
+            </blockquote>
+          </div>)}
+      </Motion>
+      )}
       <div className="controls">
         <a href={`https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="${encodeURIComponent(randomQuote?.quote)}" ${encodeURIComponent(randomQuote?.author)}`} id='tweet-quote'><FaTwitter /></a>
         <button id='new-quote' onClick={handleQuote}>Get a new quote</button>
